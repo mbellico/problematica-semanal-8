10c64e06886c96c05e0cb624e3981a2220f2bf51
Melisa D. Bellico <melisabellico@gmail.com>
2021-03-28 20:57:15 -0300

    indentation errors fixed

--- js-excercise-1/variables-and-operators.js
+++ js-excercise-1/variables-and-operators.js
@@ -3,39 +3,30 @@ Variables and Operators
 ***********************/
 /*************************************************************
 a. Crear dos variables numéricas y utilizar el operador suma
-para guardar el valor de la suma de ambos números en una tercer 
+para guardar el valor de la suma de ambos números en una tercer
 variable.
 **************************************************************/
-
 var firstNumber, secondNumber;
-
 firstNumber = 80;
 secondNumber = 20;
-
 var sum = firstNumber + secondNumber;
 console.log(sum);
 
-
 /*****************************************************************
-b. Crear dos variables de tipo String y concatenarlas guardando el 
+b. Crear dos variables de tipo String y concatenarlas guardando el
 resultado en una 3er variable.
 ******************************************************************/
-
 var firstName = 'Melisa';
 var nationality = 'Argentine';
 var personalInformation = firstName + " " + nationality;
-
 console.log(personalInformation);
 
 /**********************************************************************
-c. Crear dos variables de tipo String y sumar el largo de cada variable 
-(cantidad de letras del string) guardando el resultado la suma una 3er 
+c. Crear dos variables de tipo String y sumar el largo de cada variable
+(cantidad de letras del string) guardando el resultado la suma una 3er
 variable (utilizar length).
 **********************************************************************/
-
 var city = 'Rosario';
 var country = 'Argentina';
 var residenceLength = city.length + country.length;
-
 console.log(residenceLength);
-
--- js-excercise-2/strings.js
+++ js-excercise-2/strings.js
@@ -2,78 +2,61 @@
 Strings
 *******/
 /**************************************************************
-a. Crear una variable de tipo string con al menos 10 caracteres 
+a. Crear una variable de tipo string con al menos 10 caracteres
 y convertir todo el texto en mayúscula (utilizar toUpperCase).
-
 ***************************************************************/
-
 var fullName = 'Melisa Daniela Bellico';
 var res = fullName.toUpperCase ();
-
 console.log(res);
 
 /***************************************************************
-b. Crear una variable de tipo string con al menos 10 caracteres 
-y generar un nuevo string con los primeros 5 caracteres guardando 
+b. Crear una variable de tipo string con al menos 10 caracteres
+y generar un nuevo string con los primeros 5 caracteres guardando
 el resultado en una nueva variable (utilizar substring).
-
 ****************************************************************/
-
 var fullName = 'Melisa Daniela Bellico';
 var newStr = fullName.substring(0, 5);
-
 console.log(newStr);
 
 /***************************************************************
-c. Crear una variable de tipo string con al menos 10 caracteres 
-y generar un nuevo string con los últimos 3 caracteres guardando 
+c. Crear una variable de tipo string con al menos 10 caracteres
+y generar un nuevo string con los últimos 3 caracteres guardando
 el resultado en una nueva variable (utilizar substring).
-
 ****************************************************************/
-
 var fullName = 'Melisa Daniela Bellico';
 var newStrtwo = fullName.substring(19, 22);
-
 console.log(newStrtwo);
 
 /***************************************************************
-d. Crear una variable de tipo string con al menos 10 caracteres 
-y generar un nuevo string con la primera letra en mayúscula y las 
-demás en minúscula. Guardar el resultado en una nueva variable 
+d. Crear una variable de tipo string con al menos 10 caracteres
+y generar un nuevo string con la primera letra en mayúscula y las
+demás en minúscula. Guardar el resultado en una nueva variable
 (utilizar substring, toUpperCase, toLowerCase y el operador +).
-
 ****************************************************************/
-
 var fruit = 'strawberries';
 var newStrThree = fruit.substring(0,1);
-var newStrFour = fruit.substring(1); 
+var newStrFour = fruit.substring(1);
 var capAndLower = newStrThree.toUpperCase() + newStrFour.toLowerCase();
-
-
 console.log(capAndLower);
 
 /*****************************************************************
-e. Crear una variable de tipo string con al menos 10 caracteres 
-y algún espacio en blanco. Encontrar la posición del primer espacio 
+e. Crear una variable de tipo string con al menos 10 caracteres
+y algún espacio en blanco. Encontrar la posición del primer espacio
 en blanco y guardarla en una variable (utilizar indexOf).
 
 ******************************************************************/
-
 var lastPetsNames ='Luna Sam Lassai Michu';
 var blank = lastPetsNames.indexOf (" ");
-
 console.log(blank);
 
 /*****************************************************************
-f. Crear una variable de tipo string con al menos 2 palabras largas 
-(10 caracteres y algún espacio entre medio). Utilizar los métodos 
-de los ejercicios anteriores para generar un nuevo string que tenga 
-la primera letra de ambas palabras en mayúscula y las demás letras 
-en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase 
+f. Crear una variable de tipo string con al menos 2 palabras largas
+(10 caracteres y algún espacio entre medio). Utilizar los métodos
+de los ejercicios anteriores para generar un nuevo string que tenga
+la primera letra de ambas palabras en mayúscula y las demás letras
+en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase
 y el operador +).
-
 ******************************************************************/
-
 var carModel = 'mitsubishi outlander';
 var blankTwo = carModel.indexOf (" ");
 var firstLetterFirstWord = carModel.substring(0,1);
@@ -81,9 +64,5 @@ var firstLetterSecondWord = carModel.substring(11,12);
 var firstWord = carModel.substring(1,10);
 var secondWord = carModel.substring(12,20);
 var firstPlusSecondWord = firstLetterFirstWord.toUpperCase() + firstWord.toLowerCase() + " " + firstLetterSecondWord.toUpperCase() + secondWord.toLowerCase();
-
 console.log(blankTwo);
 console.log(firstPlusSecondWord);
-
-
-
